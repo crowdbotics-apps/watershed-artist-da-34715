749c87325ccb2ac369787719dcc7b332332e135c
Crowdbotics <team@crowdbotics.com>
2022-04-12 20:38:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "watershed_artist_da_34715",
+  url: "https://watershed_artist_da_34715.botics.co",
+  api: "https://watershed_artist_da_34715.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
